19a57cf406648624f8bc1284182667e2bbdff8c0
diogogoncalves <d.m.goncalves640@gmail.com>
2025-10-20 20:56:39 +0100

    Financial Goal Api Controller Fixed

--- Modules/FinancialGoal/Http/Controllers/Api/FinancialGoalController.php
+++ Modules/FinancialGoal/Http/Controllers/Api/FinancialGoalController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Modules\FinancialGoal\DataTables\FinancialGoalDataTable;
 use Modules\FinancialGoal\Http\Requests\FinancialGoalRequest;
 use Modules\FinancialGoal\Http\Resources\ErrorResponseResource;
 use Modules\FinancialGoal\Http\Resources\FinancialGoalCollection;
@@ -25,17 +26,12 @@ class FinancialGoalController extends ApiController
     /**
      * Display a listing of the resource.
      * @param Request $request
+     * @param FinancialGoalDataTable $dataTable
      * @return JsonResponse
      */
-    public function index(Request $request): JsonResponse
+    public function index(Request $request, FinancialGoalDataTable $dataTable): JsonResponse
     {
-        try {
-            $financialGoals = $this->repository->allUser($request);
-
-            return $this->ok(new FinancialGoalCollection($financialGoals));
-        } catch (\Exception $e) {
-            return $this->fail($e->getMessage(), $e, $e->getCode());
-        }
+        return $dataTable->ajax();
     }
 
     /**
